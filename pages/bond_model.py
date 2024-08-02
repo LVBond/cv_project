d9c60f693ac3cf3077b360245ae1b4af6a0028c3
kudinov <s.kudinov@list.ru>
2024-08-02 16:23:56 +0300

    luba model 2

--- pages/bond_model.py
+++ pages/bond_model.py
@@ -7,7 +7,8 @@ import numpy as np
 
 
 # Загружаем модель YOLOv5
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/bond/best.pt', force_reload=False)
+# model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/bond/best.pt', force_reload=False)
+model = torch.load('models/bond/best.pt', map_location=torch.device('cpu'))
 
 # Словарь для отображения названий классов
 class_names = {
