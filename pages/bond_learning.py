635ca209acc6f8c87225fb784fcbe2180bd89dfd
Liubov Bondarenko <69152815+LVBond@users.noreply.github.com>
2024-08-02 14:55:42 +0300

    Add files via upload

---
+++ pages/bond_learning.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+
+
+st.title('Обучение модели YOLAv5')
+st.divider()
+
+st.subheader('Метрики')
+st.write('Модель YOLAv5 обучалась 5 эпох на предсказание 2 классов')
+
+st.image('images/bond/m.png', use_column_width=True)
+
+
+
+
+
